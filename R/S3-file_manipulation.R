bd8894fc053db0c378b4a5a3332c3743082c26f4
harell <harell@users.noreply.github.com>
2022-06-14 20:35:00 +1200

    .dir_delete

--- R/S3-file_manipulation.R
+++ R/S3-file_manipulation.R
@@ -13,14 +13,10 @@ S3$set(which = "private", name = ".file_delete", overwrite = TRUE, value = funct
 
 
 S3$set(which = "private", name = ".dir_delete", overwrite = TRUE, value = function(path){
-    # browser()
-    # if(!self$dir_exists(path)) events$FAILED_REMOVING_DIR(path)
-    #
-    # bucket <- path |> private$extract_bucket()
-    # key <- path |> private$extract_key()
-    #
-    # conn <- private$conn
-    # conn$delete_object(bucket, key)
+    if(!self$dir_exists(path)) events$FAILED_REMOVING_DIR(path)
+
+    files <- self$dir_ls(path)
+    purrr::walk(files, self$file_delete)
 
     return(path)
 })
