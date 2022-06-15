e5c29e55246dda9dc346b5bf4cd7bf88db94dea5
harell <harell@users.noreply.github.com>
2022-06-15 21:20:29 +1200

    add sync mechanism

--- R/fs-events.R
+++ R/fs-events.R
@@ -12,4 +12,4 @@ events$FAILED_FINDING <- function(path) stop("[\033[31mx\033[39m] Failed to find
 events$COPIED_FILE <- function(path) message("[\033[32mv\033[39m] Copied ", path)
 events$SKIPPED_FILE <- function(path) message("[\033[34mi\033[39m] Skipped ", path)
 events$FAILED_REMOVING_FILE <- function(path) stop("[\033[31mx\033[39m] Failed to remove ", path, ": no such file or directory", call. = FALSE)
-events$FAILED_REMOVING_DIR <- function(path) stop("[\033[31mx\033[39m]Failed to search directory ", path, ": no such file or directory", call. = FALSE)
+events$FAILED_REMOVING_DIR <- function(path) stop("[\033[31mx\033[39m] Failed to search directory ", path, ": no such file or directory", call. = FALSE)
